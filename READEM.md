1efc7f7ef95fc0553b12cb8bfad76769115819f4
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-09-26 15:55:39 +0800

    modify

--- READEM.md
+++ READEM.md
@@ -15,3 +15,15 @@ Selenium 透過網頁瀏覽器來模擬畫面，因此需要下載瀏覽器的�
 首先我們先查看目前電腦裡的 chrome 瀏覽器是什麼版本，需要下載對應版本的 webdriver。ChromeDriver 可以來[這裏](https://chromedriver.chromium.org/downloads)下載。
 
 ![](./screenshot/img01.png)
+
+## 設定欲爬取的年月日陣列
+請在程式中將以下陣列進行初始化，假設要查詢2020年1~8月的每天。
+
+```py
+# 年份
+yearList=[2020]
+# 月份
+monthList=[1,2,3,4,5,6,7,8]
+# 每月有幾天
+dayList=[31,28,31,30,31,30,31,31] 
+```
--- helper.py
+++ helper.py
@@ -17,7 +17,7 @@ def formatNumber(num):
 
 # 載入瀏覽器驅動，以 Chrome 為例。請下載與電腦所安裝的瀏覽器版本相同的對應驅動。
 # Download source: https://chromedriver.chromium.org/downloads
-browser = webdriver.Chrome('./chromedriver')
+# browser = webdriver.Chrome('./chromedriver')
 
 # 首次開啟網頁時需要點選 Accept 按鈕，因此透過 flag 變數來判斷是否首次執行。
 flag=0
@@ -34,29 +34,30 @@ queryList=[]
 for i in yearList:
     for j in monthList:
         for k in range(1, dayList[j-1]+1):
-            queryList.append(str(i)+str(formatNumber(j))+'-'+str(formatNumber(k)))
+            for hour in range(24):
+                queryList.append(str(i)+formatNumber(j)+formatNumber(k)+'-'+formatNumber(hour))
 
 # print(queryList)
 print('爬取總天數: ',len(queryList))
 
 
 
-for i in queryList:
-    # 輸入欲爬取網頁
-    url = 'https://meteologix.com/tw/satellite/taiwan/stop-alert-10min/'+str(i)+'00z.html'
-    browser.get(url)
+# for i in queryList:
+#     # 輸入欲爬取網頁
+#     url = 'https://meteologix.com/tw/satellite/taiwan/stop-alert-10min/'+str(i)+'00z.html'
+#     browser.get(url)
 
-    # 判斷是否首次執行(首次開啟網頁需要點選Anncept按鈕)
-    if flag==0:
-        time.sleep(1)
-        browser.find_element_by_css_selector('.nx3Fpp8U.nx3gnDVX').click()
-        flag=1
-    time.sleep(0.5)
-    # city name disable by class name
-    browser.find_element_by_xpath('/html/body/div[1]/div/div[6]/div/div[1]/div/div/div[1]/div[1]/div[3]/div[1]/div[13]/div[9]/button[4]').click() 
-    # click menu by xpath
-    browser.find_element_by_css_selector('.btn-group').click() 
-    time.sleep(0.5)
-    # click download image by xpath
-    browser.find_element_by_xpath('/html/body/div[1]/div/div[6]/div/div[1]/div/div/div[1]/div[1]/div[3]/div[1]/div[1]/div/div/ul/li[3]/span[2]').click() 
-    print(str(i), ' done!')
+#     # 判斷是否首次執行(首次開啟網頁需要點選Anncept按鈕)
+#     if flag==0:
+#         time.sleep(1)
+#         browser.find_element_by_css_selector('.nx3Fpp8U.nx3gnDVX').click()
+#         flag=1
+#     time.sleep(0.5)
+#     # city name disable by class name
+#     browser.find_element_by_xpath('/html/body/div[1]/div/div[6]/div/div[1]/div/div/div[1]/div[1]/div[3]/div[1]/div[13]/div[9]/button[4]').click() 
+#     # click menu by xpath
+#     browser.find_element_by_css_selector('.btn-group').click() 
+#     time.sleep(0.5)
+#     # click download image by xpath
+#     browser.find_element_by_xpath('/html/body/div[1]/div/div[6]/div/div[1]/div/div/div[1]/div[1]/div[3]/div[1]/div[1]/div/div/ul/li[3]/span[2]').click() 
+#     print(str(i), ' done!')
